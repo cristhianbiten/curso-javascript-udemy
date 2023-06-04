133416c06b448ae54722d5a0d87e5c4054d3f9b7
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-06-04 20:28:08 -0300

    add aula 19 e exercicio aula 20

---
+++ aula19/index.js
@@ -0,0 +1,7 @@
+/*
+
+Primitivos (imutáveis) - string, number, boolean, undefined, null (bigint, symbol) - Valores copiados
+
+Referencia (mutável) - array, object, function - Passados por referência
+
+*/
