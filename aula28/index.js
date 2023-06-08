94ba2108bd2e38d7bbeffe597c9f0f3eb1034539
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-06-08 19:07:35 -0300

    Objeto Date

---
+++ aula28/index.js
@@ -0,0 +1,9 @@
+const data = new Date();
+console.log('Dia', data.getDate());
+console.log('Mês', data.getMonth() + 1); 
+console.log('Ano', data.getFullYear());
+console.log('Hora', data.getHours());
+console.log('Min', data.getMinutes());
+console.log('Seg', data.getSeconds());
+console.log('dia da semana', data.getDay());
+console.log(data.toString());
