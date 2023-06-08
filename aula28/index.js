2a8716c40662f6bc281bdaf2dfd0c07748050552
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-06-08 19:33:12 -0300

    aula 29 - Switch/case

--- aula28/index.js
+++ aula28/index.js
@@ -1,9 +1,9 @@
 const data = new Date();
 console.log('Dia', data.getDate());
-console.log('Mês', data.getMonth() + 1); 
+console.log('Mês', data.getMonth() + 1); // Mês começa do zero
 console.log('Ano', data.getFullYear());
 console.log('Hora', data.getHours());
 console.log('Min', data.getMinutes());
 console.log('Seg', data.getSeconds());
-console.log('dia da semana', data.getDay());
+console.log('dia da semana', data.getDay()); // 0 - Domingo, 6 - Sábado
 console.log(data.toString());
