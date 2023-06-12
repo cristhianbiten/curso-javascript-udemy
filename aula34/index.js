4e99a4bdaa9066a0cf219c9c98ac9775d174d002
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-06-12 15:12:13 -0300

    For clássico

---
+++ aula34/index.js
@@ -0,0 +1,8 @@
+for (let i = 0; i <= 5; i++) {
+    console.log(`Linha ${i}`)
+}
+
+const frutas = ['Maçã', 'Pêra', 'Uva'];
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}`, frutas[i])
+}
