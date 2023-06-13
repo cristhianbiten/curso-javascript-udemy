dcb51b5938cc8fed263c830896e4116e5328d902
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-06-13 13:34:45 -0300

    aula 37 - for of

---
+++ aula37/index.js
@@ -0,0 +1,16 @@
+const nome = 'Cris';
+for (let valor of nome) {
+    console.log(valor);
+}
+
+console.log('-------------------');
+
+const nomes = ['Cris', 'João', 'Maria'];
+for (let valor of nomes) {
+    console.log(valor);
+}
+
+console.log('-------------------');
+
+const frutas = ['Pera', 'Maçã', 'Uva'];
+frutas.forEach( x => console.log(x));
